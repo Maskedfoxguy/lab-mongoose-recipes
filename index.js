1bbf975cf342107a97f4ad0a0bf1b7cdf5ce9240
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-12-14 20:48:47 +0100

    submitable

--- index.js
+++ index.js
@@ -17,6 +17,44 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const myrecipe = {
+      "title": "Chicken Paprikash",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "2 tablespoon porklad ",
+        "3 pounds of Chicken",
+        "2 medium onions, very finely chopped",
+        "2 cloves of garlic, finely minced",
+        "2 Roma tomatoes, finely diced",
+        "1 HUngarian bell pepper, diced",
+        "salt and pepperto taste",
+        "4 tablespoons of hungarian sweet paprika powder"
+      ],
+      "cuisine": "Hungarian",
+      "dishType": "main_course",
+      "image": "https://www.oliviascuisine.com/wp-content/uploads/2020/12/chicken-paprikash-720x1080.jpg", 
+      "duration": 60,
+      "creator": "Chef Edina"
+    }
+    Recipe.create(myrecipe)
+    .then((createdRecipe)=> {
+      console.log(createdRecipe.title)
+    })
+    Recipe.insertMany(data)
+    .then((createdRecipes)=> {
+      console.log(createdRecipes)
+      for (let i = 0 ; i < createdRecipes.length ; i++) {
+        const currentElement = createdRecipes[i]
+        console.log(currentElement.title)
+      }
+    })
+    Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true} ,(error, data) => {
+      if(error){
+        console.log(error)
+      }else{
+        console.log(data)
+      }
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
